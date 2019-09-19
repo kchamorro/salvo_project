8f318ea8019fa160454b3042b6a5e0bc10a257a5
kchamorro <margys21@gmail.com>
2019-09-18 23:49:21 -0300

    Tarea 2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,7 @@
-INSERT INTO PLAYER (ID, FIRSTNAME, LASTNAME, USERNAME) VALUES (1,'Jack', 'Bauer', 'j.bauer@ctu.gov')
-INSERT INTO PLAYER (ID, FIRSTNAME, LASTNAME, USERNAME) VALUES (2,'Chloe','OBrian', 'c.obrian@ctu.gov')
-INSERT INTO PLAYER (ID, FIRSTNAME, LASTNAME, USERNAME) VALUES (3,'Kim', 'Bauer', 'kim_bauer@gmail.com')
-INSERT INTO PLAYER (ID, FIRSTNAME, LASTNAME, USERNAME) VALUES (4,'Tony', 'Almeida', 't.almeida@ctu.gov')
+INSERT INTO PLAYER (ID, FIRST_NAME, LAST_NAME, USER_NAME) VALUES (1,'Jack', 'Bauer', 'j.bauer@ctu.gov')
+INSERT INTO PLAYER (ID, FIRST_NAME, LAST_NAME, USER_NAME) VALUES (2,'Chloe','OBrian', 'c.obrian@ctu.gov')
+INSERT INTO PLAYER (ID, FIRST_NAME, LAST_NAME, USER_NAME) VALUES (3,'Kim', 'Bauer', 'kim_bauer@gmail.com')
+INSERT INTO PLAYER (ID, FIRST_NAME, LAST_NAME, USER_NAME) VALUES (4,'Tony', 'Almeida', 't.almeida@ctu.gov')
+
 
 
