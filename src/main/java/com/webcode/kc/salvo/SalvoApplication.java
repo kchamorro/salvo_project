a8c4eb9287e4f113496bb872e7137dce0f3b46e4
kchamorro <margys21@gmail.com>
2019-12-13 18:40:24 -0300

    Tarea 2: puntos 6-add player y punto 7:create list web page Actualización tareas módulo 4

--- src/main/java/com/webcode/kc/salvo/SalvoApplication.java
+++ src/main/java/com/webcode/kc/salvo/SalvoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.*;
@@ -25,6 +26,7 @@ import java.util.Date;
  */
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.webcode.kc.salvo"})
 public class SalvoApplication {
 
 	public static void main(String[] args) {
